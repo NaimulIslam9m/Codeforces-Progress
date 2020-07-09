8ac814053f1f883c93969a093efa5f69143f134b
Naimul Islam <naim2018331019@gmail.com>
2020-07-09 06:23:07 +0600

    progressbar on graphview added

--- app/src/main/java/com/example/CF_Progress/DataActivity.java
+++ app/src/main/java/com/example/CF_Progress/DataActivity.java
@@ -1,8 +1,11 @@
 package com.example.CF_Progress;
 
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -53,10 +56,15 @@ public class DataActivity extends AppCompatActivity {
     private ArrayList<IScatterDataSet> scatterDataSets = new ArrayList<>();
     private ScatterData scatterData;
 
+    private LinearLayout LLProgressBar, LLData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
+
+        LLProgressBar = findViewById(R.id.LLProgressBarId);
+        LLData = findViewById(R.id.LLDataId);
 
         // for passing data from one layout to another
         Bundle bundle = getIntent().getExtras();
@@ -69,7 +77,90 @@ public class DataActivity extends AppCompatActivity {
 
         retrofitFun();
         init();
-        getStatus();
+        new Task().execute();
+    }
+
+    class Task extends AsyncTask<String, Integer, Boolean> {
+        @Override
+        protected void onPreExecute() {
+            LLProgressBar.setVisibility(View.VISIBLE);
+            LLData.setVisibility(View.GONE);
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Boolean doInBackground(String... strings) {
+            Call<UserStatus> call = apiInterfaceGS.getUserStatus(handle, FROM, COUNT);
+            call.enqueue(new Callback<UserStatus>() {
+
+                @RequiresApi(api = Build.VERSION_CODES.M)
+                @Override
+                public void onResponse(Call<UserStatus> call, Response<UserStatus> response) {
+
+                    List<Result> results = response.body().getResults();
+
+                    // to get oldest to newest sumbission
+                    Collections.reverse(results);
+
+                    for (Result result : results) {
+                        if (result.getVerdict().equals("OK")) {
+                            y = result.getProblem().getRating(); // rating
+                            if (y >= 800 && y <= 3500) {
+                                entries.get(y / 100 - 8).add(new Entry(x, y));
+                                x++;
+                            }
+                        }
+                    }
+
+                    int[] ratingArray = getResources().getIntArray(R.array.cf_lvl);
+
+                    for (int i = 0; i < 27; i++) {
+                        if (entries.get(i).size() > 0) {
+                            ScatterDataSet sds = new ScatterDataSet(entries.get(i), "");
+                            sds.setColor(ratingArray[i]);
+                            sds.setScatterShape(ScatterChart.ScatterShape.CIRCLE);
+                            sds.setScatterShapeSize(12f);
+                            scatterDataSets.add(sds);
+                        }
+                    }
+
+                    scatterData = new ScatterData(scatterDataSets);
+                    scatterChart.setData(scatterData);
+
+                    // set scrollable and scalable
+                    scatterChart.setDragEnabled(true);
+                    scatterChart.setScaleEnabled(true);
+
+                    scatterChart.animateXY(0, 5000, Easing.EaseOutBounce, Easing.EaseOutBounce);
+                    scatterChart.invalidate();
+
+                    scatterChart.getDescription().setEnabled(false);
+                    scatterChart.getLegend().setEnabled(false);
+
+                    axisProperties();
+                }
+
+                @Override
+                public void onFailure(Call<UserStatus> call, Throwable t) {
+                    Log.d(TAG, "onFailure: " + t.getMessage());
+                }
+            });
+
+            try {
+                Thread.sleep(3000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            LLProgressBar.setVisibility(View.GONE);
+            LLData.setVisibility(View.VISIBLE);
+            super.onPostExecute(aBoolean);
+        }
     }
 
     private void retrofitFun() {
@@ -87,65 +178,6 @@ public class DataActivity extends AppCompatActivity {
             entries.add(new ArrayList());
         }
     }
-
-    private void getStatus() {
-        Call<UserStatus> call = apiInterfaceGS.getUserStatus(handle, FROM, COUNT);
-        call.enqueue(new Callback<UserStatus>() {
-
-            @RequiresApi(api = Build.VERSION_CODES.M)
-            @Override
-            public void onResponse(Call<UserStatus> call, Response<UserStatus> response) {
-
-                List<Result> results = response.body().getResults();
-
-                // to get oldest to newest sumbission
-                Collections.reverse(results);
-
-                for (Result result : results) {
-                    if (result.getVerdict().equals("OK")) {
-                        y = result.getProblem().getRating(); // rating
-                        if (y >= 800 && y <= 3500) {
-                            entries.get(y / 100 - 8).add(new Entry(x, y));
-                            x++;
-                        }
-                    }
-                }
-
-                int[] ratingArray = getResources().getIntArray(R.array.cf_lvl);
-
-                for (int i = 0; i < 27; i++) {
-                    if (entries.get(i).size() > 0) {
-                        ScatterDataSet sds = new ScatterDataSet(entries.get(i), "");
-                        sds.setColor(ratingArray[i]);
-                        sds.setScatterShape(ScatterChart.ScatterShape.CIRCLE);
-                        sds.setScatterShapeSize(12f);
-                        scatterDataSets.add(sds);
-                    }
-                }
-
-                scatterData = new ScatterData(scatterDataSets);
-                scatterChart.setData(scatterData);
-
-                // set scrollable and scalable
-                scatterChart.setDragEnabled(true);
-                scatterChart.setScaleEnabled(true);
-
-                scatterChart.animateXY(0, 5000, Easing.EaseOutBounce, Easing.EaseOutBounce);
-                scatterChart.invalidate();
-
-                scatterChart.getDescription().setEnabled(false);
-                scatterChart.getLegend().setEnabled(false);
-
-                axisProperties();
-            }
-
-            @Override
-            public void onFailure(Call<UserStatus> call, Throwable t) {
-                Log.d(TAG, "onFailure: " + t.getMessage());
-            }
-        });
-    }
-
 
     private void axisProperties() {
         scatterChart.getAxisRight().setEnabled(false);
