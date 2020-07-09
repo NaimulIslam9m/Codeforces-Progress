af6314531358e9ab1ce8b578390cb80e5188d361
Naimul Islam <naim2018331019@gmail.com>
2020-07-09 07:14:23 +0600

    alertDialog added

--- app/src/main/java/com/example/CF_Progress/MainActivity.java
+++ app/src/main/java/com/example/CF_Progress/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.CF_Progress;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
@@ -33,6 +35,29 @@ public class MainActivity extends AppCompatActivity {
         viewPagerAdapter.addFragment(new Fragment1(), "User Data"); // frag1
         viewPagerAdapter.addFragment(new Fragment2(), "Problems"); // frag2
         viewPager.setAdapter(viewPagerAdapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Do you want to exit?");
+
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // do anything!
+            }
+        });
+        builder.setCancelable(false);
+        builder.show();
     }
 
 }
