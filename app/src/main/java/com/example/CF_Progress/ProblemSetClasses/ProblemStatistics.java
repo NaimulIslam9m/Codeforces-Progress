6d48c99c2155d571f7a46548bfac38e3a01aba30
Naimul Islam <naim2018331019@gmail.com>
2020-07-19 13:28:42 +0600

    documented

--- app/src/main/java/com/example/CF_Progress/ProblemSetClasses/ProblemStatistics.java
+++
@@ -1,9 +0,0 @@
-package com.example.CF_Progress.ProblemSetClasses;
-
-public class ProblemStatistics {
-    private int solvedCount;
-
-    public int getSolvedCount() {
-        return solvedCount;
-    }
-}
